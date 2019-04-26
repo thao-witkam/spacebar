473d642279cb1bcdf1399dc2a46c87b5c41b9765
ThaoNguyen <thao.nguyen@telefuture.com>
2019-04-26 12:19:08 +0200

    bash

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class ArticleController extends AbstractController
+{
+    /**
+     * @Route("/")
+     *
+     * @return Response
+     */
+    public function homepage()
+    {
+        return new Response('welcome to our first page');
+    }
+
+    /**
+     * @Route("/news/{slug}")
+     */
+    public function show($slug)
+    {
+        return $this->render('article/show.html.twig', [
+            'title' => ucfirst($slug)
+        ]);
+    }
+}
