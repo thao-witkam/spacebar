b77ffa291a16957d737eecf436f0ed3a7fa96cc4
ThaoNguyen <thao.nguyen@telefuture.com>
2019-05-09 11:40:41 +0200

    make:auth LoginFormAuthenticator

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use mysql_xdevapi\Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
@@ -22,5 +24,25 @@ class SecurityController extends AbstractController
             'last_username' => $lastUsername,
             'error' => $error
         ]);
+    }
+
+    /**
+     * @Route("/login", name="app_login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout(){
+        throw new Exception('logout');
     }
 }
